b62db26713ad3b828e5ad9927ca3aee07f03c4b4
ghigbie <gehigbie@gmail.com>
2019-03-21 20:07:12 -0600

    Edit routes

---
+++ app/routes.py
@@ -0,0 +1,29 @@
+from app import app
+from flask import render_template
+
+title = "Microblog"
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Miguel'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+                {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('home.html', title=title, user=user, posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title=title)
