b5187dd4a6f38cce79c367b76fcbfd792fd71503
George Higbie <ghigbie@monigle.com>
2019-03-29 16:21:38 -0600

    Edit login route again

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
+from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, current_user
 from app import app
-from flask import render_template, flash, redirect
-from flask_login import current_user, login_user
 from app.forms import LoginForm
 from app.models import User
 
