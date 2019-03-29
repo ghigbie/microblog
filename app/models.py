8cf55cc4993f2c798800a8a63e06047c0c436614
George Higbie <ghigbie@monigle.com>
2019-03-29 15:54:24 -0600

    Edit user

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db     
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -18,6 +18,11 @@ class User(db.Model):
     
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(id)
 
 
 class Post(db.Model):
