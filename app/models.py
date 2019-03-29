e89f7c29cee6e101189371a30727de0caae3a195
George Higbie <ghigbie@monigle.com>
2019-03-29 15:45:44 -0600

    Edit password function

--- app/models.py
+++ app/models.py
@@ -9,13 +9,14 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     post = db.relationship('Post', backref='author', lazy='dynamic')
+    
     def __repr__(self):
         return f'<User {self.username}>'
     
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
     
-    def check_password_hash(self, password):
+    def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 
