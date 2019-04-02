e053b4483ba9b6a8f8f9e3add3327c2455562843
ghigbie <gehigbie@gmail.com>
2019-04-01 21:55:08 -0600

    Add registration link

--- app/routes.py
+++ app/routes.py
@@ -48,10 +48,14 @@ def login():
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
+    return render_template('login.html', app_title=app_title, title='Sign In', form=form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('logout'))
+
+@app.route('/register')
+def register():
+    title = 'Registration'
+    return render_template('register.html', app_title=app_title, title=title)
